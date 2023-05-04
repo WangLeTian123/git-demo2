f87b4ac238043f17181be1a1cd5bdf80ed581c46
WangLeTian123 <127586544+WangLeTian123@users.noreply.github.com>
2023-05-04 17:15:52 +0800

    Update HelloWorld.java
    again

--- HelloWorld/src/main/java/org/letian/HelloWorld.java
+++ HelloWorld/src/main/java/org/letian/HelloWorld.java
@@ -3,5 +3,7 @@ package org.letian;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
