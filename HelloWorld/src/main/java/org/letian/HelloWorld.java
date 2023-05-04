d62b54192f380b27b83cc9136fe0c865baec50aa
letian <letian@123.com>
2023-05-04 17:23:41 +0800

    letian 3 commit

--- HelloWorld/src/main/java/org/letian/HelloWorld.java
+++ HelloWorld/src/main/java/org/letian/HelloWorld.java
@@ -2,9 +2,8 @@ package org.letian;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello LeTian");
+        System.out.println("Main");
+        System.out.println(123);
+        System.out.println(1234);
     }
 }
