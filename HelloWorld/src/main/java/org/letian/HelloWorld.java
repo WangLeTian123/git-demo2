77928ba3a7e3621b96ed5d00296cf485a2414be8
letian <2606585912@qq.com>
2023-05-06 15:48:49 +0800

    centos first commit

--- HelloWorld/src/main/java/org/letian/HelloWorld.java
+++ HelloWorld/src/main/java/org/letian/HelloWorld.java
@@ -4,13 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Main");
         System.out.println(123);
-        System.out.println(1234);
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
         System.out.println();
         System.out.println(32234);
     }
